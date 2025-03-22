dd31639decc4731fcb5cd8b76b2f5fe65f777466
lkr9 <94123314+lkr9@users.noreply.github.com>
2025-03-22 12:04:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+constructor(name ,age){
+this.name=name;
+this.age=age;
 
-class Student extends Person {}
+}
+getname(){
+return this.name;
+}
+setname(n){
+this.name=n}
+getage(){
+return this.age}
+setage(a){
+this.age=a}
 
-class Teacher extends Person {}
+}
+class Student extends Person {
+ study() {
+        console.log(`${this.name} is studying`); 
+ }
+}
+
+class Teacher extends Person {
+ teach() {
+        console.log(`${this.name} is teaching`); 
+ }
+}
 
 // Do not change the code below this line
 window.Person = Person;
